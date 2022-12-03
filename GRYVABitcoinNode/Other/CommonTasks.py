326dd95575a0140c256be2226a6dfb455d245c84
Marius Göcke <marius.goecke@gmail.com>
2022-12-04 00:25:04 +0100

    update

--- GRYVABitcoinNode/Other/CommonTasks.py
+++ GRYVABitcoinNode/Other/CommonTasks.py
@@ -10,10 +10,15 @@ def common_tasks():
     file = str(Path(__file__).absolute())
     folder_of_current_file = os.path.dirname(file)
     sc = ScriptCollectionCore()
-    version = sc.get_semver_version_from_gitversion(GeneralUtilities.resolve_relative_path("../..", os.path.dirname(file)))
-    sc.replace_version_in_dockerfile_file(GeneralUtilities.resolve_relative_path("../GRYVABitcoinNode/Dockerfile", folder_of_current_file), version)
+    cmd_args = sys.argv
     t = TasksForCommonProjectStructure()
-    t.standardized_tasks_do_common_tasks(file, version, 1, "QualityCheck", True, None, sys.argv)
+    verbosity = t.get_verbosity_from_commandline_arguments(cmd_args, 1)
+    targetenvironmenttype = t.get_targetenvironmenttype_from_commandline_arguments(cmd_args, "QualityCheck")
+    additional_arguments_file = t.get_additionalargumentsfile_from_commandline_arguments(cmd_args, None)
+    codeunit_version = sc.get_semver_version_from_gitversion(GeneralUtilities.resolve_relative_path(
+        "../..", os.path.dirname(file)))  # Should always be the same as the project-version
+    sc.replace_version_in_dockerfile_file(GeneralUtilities.resolve_relative_path("../GRYVABitcoinNode/Dockerfile", folder_of_current_file), codeunit_version)
+    t.standardized_tasks_do_common_tasks(file, codeunit_version, verbosity, targetenvironmenttype, True, additional_arguments_file, cmd_args)
 
 
 if __name__ == "__main__":
