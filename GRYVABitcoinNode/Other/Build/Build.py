3924a949751da06d52c336b9d1bcaffed9b286ed
Marius Göcke <marius.goecke@gmail.com>
2024-10-17 19:14:55 +0200

    updated script

--- GRYVABitcoinNode/Other/Build/Build.py
+++ GRYVABitcoinNode/Other/Build/Build.py
@@ -1,10 +1,18 @@
 import sys
+import os
 from pathlib import Path
+from ScriptCollection.GeneralUtilities import GeneralUtilities
 from ScriptCollection.TasksForCommonProjectStructure import TasksForCommonProjectStructure
 
 
 def build():
-    TasksForCommonProjectStructure().standardized_tasks_build_for_docker_project(str(Path(__file__).absolute()), "QualityCheck", 1, sys.argv)
+    script_file = str(Path(__file__).absolute())
+    dependency_folder = GeneralUtilities.resolve_relative_path("../../Resources/Dependencies/Bitcoin", script_file)
+    version_file = os.path.join(dependency_folder, "Version.txt")
+    bitcoin_version = GeneralUtilities.read_text_from_file(version_file)
+    arguments = dict[str, str]()
+    arguments["BitcoinVersion"] = bitcoin_version
+    TasksForCommonProjectStructure().standardized_tasks_build_for_docker_project_with_additional_build_arguments(str(Path(__file__).absolute()), "QualityCheck", 1, sys.argv, arguments)
 
 
 if __name__ == "__main__":
